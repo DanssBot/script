268d224a2bcfc731b9a106814eec938fd7b516d1
DanssBot <90471487+DanssBot@users.noreply.github.com>
2021-10-06 05:36:07 -0500

    Update zzupdate.sh

--- generadores/control/zzupdate.sh
+++ generadores/control/zzupdate.sh
@@ -195,7 +195,7 @@ printTitle "Tiempo que tomó Actualizacion"
 echo "$((($(date +%s)-$TIME_START)/60)) min."
 msg -bar2
 echo -e "\033[93m           -- ACTUALIZACION CASI COMPLETA -- "
-wget https://raw.githubusercontent.com/diesel09/script/master/generadores/control/VPS-MX.sh -O /usr/bin/VPS-MX &> /dev/null
+wget https://raw.githubusercontent.com/DanssBot/script/master/generadores/control/VPS-MX.sh -O /usr/bin/VPS-MX &> /dev/null
 chmod +x /usr/bin/VPS-MX
 if [ "$REBOOT" = "1" ]; then
 	printTitle "        SU VPS SE REINICIARA EN 20 SEGUNDOS           "
